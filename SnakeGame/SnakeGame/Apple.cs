7253aaa454cc54da9ab1bb9807416e220ecfcf9c
hazal <hazal.iscen@hotmail.com>
2018-02-15 08:48:08 +0100

    initial push

---
+++ SnakeGame/SnakeGame/Apple.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Windows.Controls;
+
+namespace SnakeGame
+{
+    class Apple
+    {
+        public double x,y;
+        public Rectangle poli = new Rectangle();
+        public Apple(double x,double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void setfoodposition()
+        {
+            poli.Width = poli.Height = 15;
+            poli.Fill = Brushes.Red;
+            Canvas.SetLeft(poli, x);
+            Canvas.SetTop(poli,y);
+        }
+    }
+}
---
+++ SnakeGame/SnakeGame/Snake.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace SnakeGame
+{
+    class Snake
+    {
+        public double x;
+        public double y;
+        public Rectangle polygon = new Rectangle();
+        public Snake(double x = 100,double y = 100)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void setsnakeposition()
+        {
+            polygon.Width = polygon.Height = 15;
+            polygon.Fill = Brushes.White;
+            Canvas.SetLeft(polygon, x);
+            Canvas.SetTop(polygon, y);
+        }
+    }
+}
