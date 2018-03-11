e3a11489ceec6375394d80b03b27445a1097c4d1
Niggo Betzler <n.betzler@yahoo.de>
2018-03-11 21:34:12 +0100

    Add files via upload

---
+++ TCP_IP_Connection/TCP_IP_Connection/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TCP_IP_Connection
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Server tcpsv = new Server();
+            new Client().StartClient();
+        }
+
+        private void Btn_send_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] toSend = new byte[1024];
+            for (int i = 0; Txtbl_input.Text[i-1] != '\0'; i++)
+            {
+                toSend[i] = (byte)Txtbl_input.Text[i];
+            }
+        }
+    }
+}
